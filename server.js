a929f777a9c34c262831fb3aeb6a9e4e570ba78f
H Dusterhoft <119828145+h-dusterhoft@users.noreply.github.com>
2023-09-27 12:23:32 -0500

    Start api to get restaurants

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ const express = require('express');
 
 const app = express();
 
-app.get('/restaurants', (req, res) => {
-    res.send('these are the restaurants');
+// get all restaurants
+app.get('/api/v1/restaurants', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            restaurants: ['McDonalds', 'Wendys'],
+        },
+    });
 });
 
-
+// get individual restaurant
+app.get('/api/v1/restaurants/:id', (req, res) => {
+    
+})
 
 const port = process.env.PORT || 3000;
 
